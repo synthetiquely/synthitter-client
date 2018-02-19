8e0cf72a19263a98854ebdb083caf7e9af921795
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2018-02-19 14:02:25 +0300

    Register button as a global component

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import Button from './components/Common/FormFields/Button.vue';
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   render: h => h(App),
 });
+
+Vue.component('Button', Button);
