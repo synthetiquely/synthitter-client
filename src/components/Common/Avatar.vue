4b5c3fda0ed32b08fb0cbff9922c083a070d892f
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2018-02-20 15:21:15 +0300

    General component for user avatar

---
+++ src/components/Common/Avatar.vue
@@ -0,0 +1,49 @@
+<template>
+  <img :src="src" alt="user avatar" class="avatar" :class="classes">
+</template>
+
+<script>
+export default {
+  name: 'Avatar',
+  props: {
+    src: {
+      type: String,
+      default:
+        'http://www.cheap-accountants-in-london.co.uk/wp-content/uploads/2015/07/User-Avatar.png',
+    },
+    size: {
+      type: Number,
+      default: 68,
+    },
+  },
+  computed: {
+    classes() {
+      return {
+        [`size-${this.size}`]: true,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.avatar {
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.avatar.size-68 {
+  width: 68px;
+  height: 68px;
+}
+
+.avatar.size-32 {
+  width: 32px;
+  height: 32px;
+}
+
+.avatar.size-40 {
+  width: 40px;
+  height: 40px;
+}
+</style>
